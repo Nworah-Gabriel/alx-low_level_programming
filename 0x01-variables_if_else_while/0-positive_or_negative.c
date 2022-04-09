1654d0a2826dd54a7d19279f61990f41a6554f85
Nworah-Gabriel <gabrielnworah6@gmail.com>
2022-04-08 22:06:43 -0700

    My project commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return:  Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0){
+		printf("the number: %d is positive", n);
+	}
+	else if (n == 0){
+		printf("the number: %d is zero", n);
+	}
+	else if (n < 0){
+		printf("the number: %d is negative", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+This folder contains a script for c programms with if else and while statement
+it's an ALX SE project
