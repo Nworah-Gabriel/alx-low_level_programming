d5a4e035f4144da72f1adc5978e378e23fb441d0
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2022-05-10 14:30:31 +0100

    Update main.h
    Fixed

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -31,27 +31,6 @@ int error_exit(void)
 	_putchar('\n');
 	exit(98);
 }
-
-/**
- * check_number - checks if string has only
- *                numbers
- *
- * @str: string to check
- *
- * Return: 0 is true 1 if false
-*/
-
-int check_number(char *str)
-{
-	while (*str != '\0')
-	{
-		if (*str < '0' || *str > '9')
-			return (1);
-		str++;
-	}
-	return (0);
-}
-
 /**
  * _length - get the length of strings
  *
