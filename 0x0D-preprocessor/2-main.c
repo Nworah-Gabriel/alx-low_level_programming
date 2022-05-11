8f618f444f62444792c034083a0d75decd2a5263
Nworah-Gabriel <gabrielnworah6@gmail.com>
2022-05-11 15:41:24 -0700

    Hot fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,12 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
+
+/**
+ * main - prints file name
+ * Return: Always 0
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
