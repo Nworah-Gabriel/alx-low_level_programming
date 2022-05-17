0b33db2242fef23a423db4c72cc569f34f5e9853
Nworah-Gabriel <gabrielnworah6@gmail.com>
2022-05-17 07:33:42 -0700

    my commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_fuctions.h"
+#include  <stdarg.h>
+
+/**
+  * sum_them_all - A function that returns the sum of digits
+  * @n: input for maximum number of the function parameters
+  * @...: elipses for the vardiac function
+  *Return: always 0
+  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	va_list mylist;
+	int sum = 0;
+	int i;
+
+	va_start(mylist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(mylist, const unsigned int);
+	}
+	va_end(mylist);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+My practice on variadic function
