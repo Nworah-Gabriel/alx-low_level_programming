7f3cc2345f371519ccfdc80f3c83cf445e5d67b6
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2022-05-17 15:52:07 +0100

    Update 0-sum_them_all.c
    Fixed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,26 @@
-#include "variadic_fuctions.h"
+#include "variadic_functions.h"
 
 /**
-  * sum_them_all - A function that returns the sum of digits
-  * @n: input for maximum number of the function parameters
-  * @...: elipses for the vardiac function
-  *Return: always 0
-  */
-
+ * sum_them_all - A function that returns the sum of all its parameters.
+ * @n: number of the arguments.
+ *
+ * Return: sum of the argument.
+ */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list mylist;
+	unsigned int i;
+	int sum = 0;
+
 	if (n == 0)
 		return (0);
-
-	va_list mylist;
-	int sum = 0;
-	Unsigned int i;
 
 	va_start(mylist, n);
 
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(mylist, int);
-	}
+
 	va_end(mylist);
+
 	return (sum);
 }
