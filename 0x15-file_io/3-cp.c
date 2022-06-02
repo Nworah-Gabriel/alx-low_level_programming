9e70b1189dfb214df24f73dc488fb69e6b89acc4
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2022-06-03 00:29:46 +0100

    Update 3-cp.c
    Updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,72 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-
-char *create_buffer(char *file);
-void close_file(int fd);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-char *create_buffer(char *file)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
-
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
-/**
- *main - a function that recieves parameters and copies file content
- from.one file to the other
- *@argc: file argument count
- *@argv: file arguments
- *Return: return 0 on success
- */
-int main(int argc,char *argv[])
-{
-	if (argc != 3)
-	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 29);
-		exit(97);
-	}
-	int op = open(argv[2], O_CREAT | O_TRUNC | S_IWUSR | S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	int op2 = open(argv[1], O_RDONLY);
-
-	if (op2 == -1)
-	{
-		dprintf(2, "Error can't read from the file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (op == -1)
+	if (file_to == -1)
 	{
-		dprintf(2, "Error: can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
-	}	
-		char buff = create_buffer(op2);
-	char rd;
-	
-	rd = read(op2, buff, 1024);
-	wr = write(op, buff, 1024);
-	
-	close_file(op);
-	close_file(op2);
+	}
+}
 
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
